79ab9a6a113f08b446b205e8ee5dbf1f6e98cd15
Jack <->
2022-12-13 14:05:58 +0000

    Delete dead code

--- sbatch/server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#SBATCH --mem 500
-#SBATCH --partition LARGE-G2
-
-python -m http.server 5000
--- sbatch/test_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm output/miniconcode_test_preprocessed.pickle
-rm output/miniconcode_train_preprocessed.pickle
-
-python dataset.py
-
-diff output/miniconcode_test_preprocessed*
-diff output/miniconcode_train_preprocessed*
