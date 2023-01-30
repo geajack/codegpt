c49ef0cfe2ea58fe89abcc9718742ca201fba41c
Jack <->
2023-01-30 15:47:28 +0000

    Clean up

--- sbatch/predict.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#SBATCH --mem 64000
-#SBATCH --gres=gpu:1
-#SBATCH --partition LARGE-G2
-#SBATCH --output output/logs
-
-PYTHONUNBUFFERED=1 python predict.py $1
--- sbatch/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#SBATCH --mem 64000
-#SBATCH --partition LARGE-G2
-#SBATCH --output output/logs
-
-PYTHONUNBUFFERED=1 python train.py $1
