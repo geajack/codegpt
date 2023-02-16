fbec21b6dd9d5269b1c7a2cf0dc8513273590e2f
Jack <->
2023-02-16 19:59:02 +0000

    Mess with debug scripts

--- sbatch/debug.sh
+++ sbatch/debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export node=147.252.6.52
-sbatch sbatch/sbatch_wrapper.sh $@
+export node=147.252.6.51
+sbatch sbatch/sbatch_debug_wrapper.sh $@
 ssh -N -L 5678:localhost:5000 $node
--- sbatch/sbatch_debug_wrapper.sh
+++ sbatch/sbatch_debug_wrapper.sh
@@ -4,5 +4,5 @@
 #SBATCH --partition MEDIUM-G2
 #SBATCH --output output/logs
 
-# ifconfig
+ifconfig
 PYTHONPATH=. PYTHONUNBUFFERED=1 python -m debugpy --listen 5000 --wait-for-client $@
